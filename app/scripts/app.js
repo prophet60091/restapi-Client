22395d63bdd1b9ccd63fb17c4ccc80265547cf5e
Robert <rpjackson@gmail.com>
2016-10-11 23:31:13 -0600

    change ip address of serve binding

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,7 @@ angular
   ])
   .config(function ($routeProvider, RestangularProvider) {
 
-    RestangularProvider.setBaseUrl('http://54.218.249.67:9000/');
+    RestangularProvider.setBaseUrl('http://0.0.0.0:9000/');
     // RestangularProvider.setJsonp(true);
     // RestangularProvider.setDefaultRequestParams('jsonp', {callback: 'JSON_CALLBACK'});
     // RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
